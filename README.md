42112671f42e01b13db794c7a7a695525f51c134
UnaiLopez <unai19970315@gmail.com>
2022-06-12 19:46:07 +0200

    improved readme and setup.py

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# DARWIN
+![alt text](https://github.com/unaiLopez/darwin/blob/master/doc/images/darwin.jpg?raw=true)
+
+# Darwin: A genetic algorithm powered hyperparameter optimization framework
+---
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/MIT)
 
 Darwin is a high level framework that helps optimizing functions using the power of genetic algorithms.
-
-![alt text](https://github.com/unaiLopez/darwin/blob/master/doc/images/darwin.jpg?raw=true)
 
 
 
--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(
-    name='genetist',
-    version='0.1',
-    license='MIT',
-    author='Unai Lopez Ansoleaga',
-    author_email='unai19970315@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/unaiLopez/genetist',
-    keywords='genetic algorithm optimization',   
-    install_requires=[
-          'numpy', 'pandas', 'tqdm', 'logging'
-      ],
+with open('README.md') as f:
+    long_description = f.read()
 
-)
+INSTALL_REQUIRES = [
+    'numpy==1.22',
+    'tqdm==4.4.1',
+    'pandas==1.1.5',
+]
+
+if __name__ == '__main__':
+    setup(
+        name='darwin',
+        description='darwin: optimization with genetic algorithms',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Unai Lopez Ansoleaga',
+        author_email='unai19970315@gmail.com',
+        uurl='https://github.com/unaiLopez/darwin',
+        license='MIT',
+        package_dir={'': 'src'},
+        packages=find_packages('src'),
+        include_package_data=True,
+        install_requires=INSTALL_REQUIRES,
+        python_requires='>=3.6',
+    )
