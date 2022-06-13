68d7e55636b306d9b4b0a7bd3520135b526a58e5
UnaiLopez <unai19970315@gmail.com>
2022-06-13 21:29:51 +0200

    changed setup.py

--- genetist/__init__.py
+++
@@ -1,5 +0,0 @@
-from genetist import environment
-from genetist import individual
-from genetist import mutation
-from genetist import datatype_inference
-from genetist import crossover
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open('README.md') as f:
     long_description = f.read()
 
 INSTALL_REQUIRES = [
-    'numpy==1.19.0',
-    'tqdm==4.4.1',
-    'pandas==1.1.5',
+    'numpy==1.21.3',
+    'tqdm==4.64.0',
+    'pandas==1.3.4',
 ]
 
 if __name__ == '__main__':
@@ -14,14 +14,14 @@ if __name__ == '__main__':
         name='genetist',
         description='Genetist: optimization with genetic algorithms',
         long_description=long_description,
-        version=0.0.0,
+        version=0.2,
         long_description_content_type='text/markdown',
         author='Unai Lopez Ansoleaga',
         author_email='unai19970315@gmail.com',
         url='https://github.com/unaiLopez/genetist',
         license='MIT',
-        package_dir={'': 'genetist'},
-        packages=find_packages('genetist'),
+        package_dir={'': 'src'},
+        packages=find_packages('src'),
         include_package_data=True,
         install_requires=INSTALL_REQUIRES,
         python_requires='>=3.6'
